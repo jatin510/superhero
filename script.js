51952412ee26de30cb0273954edadb8b69b8d60a
Jagdish <jatin6972@gmail.com>
2020-05-31 19:26:48 +0530

    rendering in the front end

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
-var search = document.querySelector("#search");
-console.log(search);
+const search = document.querySelector("#search");
+const results = document.querySelector("#results");
 
-var searchTerm = "";
-var heroes = "";
+var heroes;
 
 const xhr = new XMLHttpRequest();
 
-// xhr.onreadystatechange = () => {};
+// using XMLHttpRequest
 
-// const fetchSearchData = async (key) => {
+// fetchHeroes = async (key) => {
+//   var xhr = new XMLHttpRequest();
 //   url = ` https://superheroapi.com/api/1469687849876867/search/${key}`;
-//   heroes = await fetch(url, {
-//     method: "GET",
-//     mode: "cors",
-//     credentials: "same-origin", // include, same-origin, *omit
-//     headers: {
-//       "Content-Type": "application/json; charset=utf-8",
-//     },
-//     redirect: "follow", // manual, *follow, error
-//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//   });
 
-//   //   .then((res) => res.json());
-//   console.log(url);
-//   console.log(heroes);
+//   xhr.onload = () => {
+//     let result = JSON.parse(xhr.response);
+//     heroes = result;
+//   };
+//   xhr.open("GET", url, false);
+//   xhr.send();
 // };
 
-fetchSearchData = (key) => {
-  var xhr = new XMLHttpRequest();
+fetchHeroes = async (key) => {
   url = ` https://superheroapi.com/api/1469687849876867/search/${key}`;
-  //   xhr.readyState = () => {
-  //     console.log("xhr");
-  //     if (this.onreadystatechange == 4 && this.status == 200) {
-  //       //   console.log(this.responseText);
-  //       console.log("hello");
-  //     }
-  //     xhr.open("GET", url, true);
-  //     // xhr.onload = success;
-  //     xhr.send();
-  //   };
+  heroes = await fetch(url).then((res) => res.json());
+};
 
-  xhr.onload = () => {
-    let result = JSON.parse(xhr.response);
-    console.log(result.results[0]);
-  };
-  xhr.open("GET", url, true);
-  xhr.send();
+const showHeroes = async (key) => {
+  //   result.innerHTML = "";
+  await fetchHeroes(key);
+
+  console.log("show ", heroes);
+
+  var ul = document.createElement("ul");
+
+  ul.classList.add("heroes");
+  heroes = heroes.results;
+  var names = heroes.filter((hero) =>
+    hero.name.toLowerCase().startsWith(key.toLowerCase())
+  );
+  console.log(names);
+  names.forEach((hero) => {
+    const li = document.createElement("li");
+
+    li.classList.add("hero-item");
+  });
 };
 
 search.addEventListener("input", (e) => {
   searchTerm = e.target.value;
   console.log(searchTerm);
 
-  fetchSearchData(searchTerm);
+  showHeroes(searchTerm);
 });
